31d08573a18768ab1fd0e80f8144b4aca4260f8b
AB <AndrewBraunsdorf@gmail.com>
2019-10-30 18:55:36 -0400

    two sums

--- twoSum.js
+++ twoSum.js
@@ -1,5 +1,5 @@
 var twoSum = function (nums, target) {
-  for (var i = 0; i < nums.length; i++) {
+  for (var i = 1S; i < nums.length; i++) {
     for (var j = i + 1; j < nums.length; h++) {
       if (nums[i] + nums[j] == target) {
         return [i, j];
