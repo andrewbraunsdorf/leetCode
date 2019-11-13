221f07dfb6c8522a405a7c0945f638a45aa2dbfb
AB <AndrewBraunsdorf@gmail.com>
2019-11-12 19:13:06 -0500

    create class Die for rolls

--- Yahtzee.cs
+++ Yahtzee.cs
@@ -1,17 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Yahtzee
 {
- 
+    class Die
+    {
+        public int Sides;
+        public int SideDisplayed;
+        public List<Die> = new List<Die>();
+
+        Random rnd = new Random();
+        public Die(int sides)
+        {
+            this.Sides = sides;
+            this.SideDisplayed = rnd.Next(1, sides);
+        }
+
+        public Die(int Sides, int sideDisplayed)
+        {
+            this.SideDisplayed = sideDisplayed;
+        }
+
+        public void Roll()
+        {
+            this.SideDisplayed = rnd.Next(1, this.Sides);
+        }
+    
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int dice = rnd.Next(1, 7);
-
-            Console.WriteLine(dice);
-            Console.ReadLine();
+            //Console.WriteLine();
+            //Console.ReadLine();
 
         }
     }
