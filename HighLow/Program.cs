5b1273859c447114a69bdadcb40eac7783e7b314
AB <AndrewBraunsdorf@gmail.com>
2019-11-18 19:39:46 -0500

    created Card class

--- HighLow/Program.cs
+++ HighLow/Program.cs
@@ -1,14 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HighLow
 {
     class Program
     {
+        public class Card
+        {
+
+            public int CardValue;
+            public int SuitValue;
+            public int RandomCardValue;
+
+            public string[] Suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            public int[] Values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+            public Card()
+            {
+                Random newCard = new Random();
+                this.CardValue = newCard.Next(1, 14);
+                this.CardValue = newCard.Next(0, 4);
+                Console.WriteLine(CardValue);
+                Console.WriteLine(SuitValue);
+            }
+
+            public Card(int RandomCardValue)
+            {
+                Random randomCardValue = new Random();
+                this.RandomCardValue = randomCardValue.Next(1, 14);
+                //Console.WriteLine("randomCardValue: {0}", randomCardValue);
+                Console.WriteLine("RandomCardValue: {0}", RandomCardValue);
+            }
+
+            public Card(int CardValue, int SuitValue)
+            {
+                this.CardValue = CardValue;
+                this.SuitValue = SuitValue;
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            Random newCard = new Random();
-            int randomCard = newCard.Next(1, 14);
-            Console.WriteLine(randomCard);
+
+            var card = new Card();
+            var cards = new Card(10);
+
             Console.ReadLine();
         }
     }
